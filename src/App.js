6200eb234313225771da1396541b612483fe205a
Paschal <58183764+paschal533@users.noreply.github.com>
2022-08-15 21:17:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+//import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/Signup';
